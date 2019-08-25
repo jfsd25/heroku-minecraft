5a042ea3f8d33e2d458a4cf154d1d38796b2cb84
jfsd25 <54447428+jfsd25@users.noreply.github.com>
2019-08-26 01:06:53 +0800

    Update postinstall.sh

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set Minecraft version
-MC_VERSION="${MC_VERSION:-1.8.3}"
+MC_VERSION="${MC_VERSION:-1.14.4}"
 
 
 # Download Minecraft
-curl -o vendor/minecraft_server.jar "https://s3.amazonaws.com/Minecraft.Download/versions/${MC_VERSION}/minecraft_server.${MC_VERSION}.jar"
+curl -o vendor/minecraft_server.jar "https://cdn.getbukkit.org/spigot/spigot-1.14.4.jar"
